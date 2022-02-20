16245c3d74bb4165df57e7a74f925f09626df46b
woo <choiwu0917@gmail.com>
2022-02-20 16:55:36 +0900

    immer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useRef,useCallback,useState} from "react";
+import produce from "immer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+  const nextId=useRef(1);
+  const [form, Setform]=useState({name:'',username:''});
+  const [data,Setdata]=useState({
+    array:[],
+    uselessValue:null
+  });
+  const onChange=useCallback(event=>{
+    const {name,value}=event.target;
+    Setform(
+      produce(form,draft=>{
+        draft[name]=value;
+      })
+    );
+  },[form]);
+  const onSubmit=useCallback(event=>{
+    event.preventDefault();
+    const info={
+      id:nextId.current,
+      name:form.name,
+      username:form.username,
+    };
+    Setdata(
+      produce(draft=>{
+        draft.array.push(info);
+      })
+    )
+    Setform({
+      name:'',
+      username:'',
+    });
+    nextId.current += 1;
+  },[form.name, form.username]);
+  const onRemove=useCallback(
+    id=>{
+      Setdata(
+        produce(data,draft=>{
+          draft.array.splice(draft.array.findIndex(info=>info.id===id),1);
+        })
+      );
+    },[data]
+  );
+  return(
+    <div>
+      <form onSubmit={onSubmit}>
+        <input name="username" placeholder="ID" value={form.username} onChange={onChange}/>
+        <input name="name" placeholder="Name" value={form.name} onChange={onChange}/>
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map(info=>(
+            <li key={info.id} onClick={()=>{onRemove(info.id)}}>
+              {info.username}({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
